154317f0dac6108453b124b5a8d17e2abfb92d67
Andre <143305136+andrehunt@users.noreply.github.com>
2024-05-21 20:17:48 +1200

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Add smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
